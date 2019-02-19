22d44b6f976ac069c6ca43aa2762c551001471a7
beemolin <chienhsuan.lin@igroup.tw>
2019-02-19 09:17:28 +0800

    change socket.io server

--- src/App.js
+++ src/App.js
@@ -1,28 +1,42 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React, { Component } from "react";
+import socketIOClient from "socket.io-client";
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      response: false,
+      endpoint: "http://59.126.167.134:88"
+    };
+  }
+  componentDidMount() {
+    const { endpoint } = this.state;
+    const socket = socketIOClient(endpoint);
+    socket.on("FromAPI", data => this.setState({ response: data }));
+  }
   render() {
+    const { response } = this.state;
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div style={{ textAlign: "center" }}>
+        {response
+          ? (
+              <div>
+                <p>
+                  伺服器分流: {response.server}
+                </p>
+                <p>
+                  現在線上使用者數量: {response.users}
+                </p>
+                <p>
+                  最大同時線上使用者數量: {response.maxUsers}
+                </p>
+                <p>
+                  現在伺服器時間: {response.currentDateTime}
+                </p>
+              </div>
+            )
+          : <p>Loading...</p>}
       </div>
     );
   }
 }
-
 export default App;
